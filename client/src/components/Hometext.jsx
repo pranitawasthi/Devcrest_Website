d9822ca537314898038d1a061df1c6a06f405a77
Pranit <pranitawasthi03@gmail.com>
2025-01-13 00:04:37 +0530

    bug fix

--- client/src/components/Hometext.jsx
+++ client/src/components/Hometext.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import "../home.css";
 
 export const Hometext = () => {
   return (
@@ -9,30 +10,22 @@ export const Hometext = () => {
     <br />
     <br />
     <br />
-  <br />
-  <h1>
-    <span>Collaborative, </span>
-    
-    <span className="txtgr">Learning-based</span>
-  </h1>
-  <h1>Technical Community</h1>
-  <br />
-  {/* <p>Lorem ipsum, dolor sit amet <span class="txtgr">dd</span> consectetur adipisicing elit. Molestias, nihil.
-  </p> */}
-  <p>
-    {" "}
-    Collaborate, build <span className="txtgr">real-world projects</span>,
-    sharpen <span className="txtgr">coding skills</span> , and embrace industry
-    practices.
-  </p>
-  <br />
-  <br />
-  <div className="buts">
-    <button className="btn">Get Started</button>
-    <button className="btn2">Watch Demo</button>
-  </div>
-  <br />
-  <br />
+      <div className="hometext-container">
+        <h1 className="main-heading">
+          <span className='coll'>Collaborative, </span> 
+          <span className="txtgr">Learning-based</span>
+        </h1>
+        <h1 className="sub-heading">Technical Community</h1>
+        <p className="description">
+          Collaborate, build <span className="txtgr">real-world projects</span>,
+          sharpen <span className="txtgr">coding skills</span>, and embrace industry
+          practices.
+        </p>
+        <div className="buts">
+          <button className="btn">Get Started</button>
+          <button className="btn2">Watch Demo</button>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
