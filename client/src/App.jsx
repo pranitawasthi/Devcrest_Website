62d5fd71e9a674d82a1265ffc75a4f55a73fe4a9
pranitawasthi <114942476+pranitawasthi@users.noreply.github.com>
2025-01-14 05:44:05 +0530

    back to og

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,7 +26,6 @@ function App() {
           <Route path="/project" element={<Projects/>} />
           <Route path="/about" element={<About/>} />
           <Route path="/event" element={<Events/>} />
-          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </div>
     </Router>
